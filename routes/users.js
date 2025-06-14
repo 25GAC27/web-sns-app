0f5ec1b70b37d559f4db12101aca4b0ca821ebef
watakazu-git <k25gac27@st.kyusan-u.ac.jp>
2025-06-14 18:14:57 +0900

    Add: フォロー解除するAPIの作成

--- routes/users.js
+++ routes/users.js
@@ -75,4 +75,35 @@ router.put('/:id/follow', async (req, res) => {
     }
 });
 
+// Unfollow a user
+router.put('/:id/unfollow', async (req, res) => {
+    if (req.body.userId != req.params.id) {
+        try {
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.body.userId);
+
+            // Check if the user is already following
+            if (user.followers.includes(req.body.userId)) {
+                await user.updateOne({
+                    $pull: {
+                        followers: req.body.userId,
+                    },
+                });
+                await currentUser.updateOne({
+                    $pull: {
+                        followings: req.params.id,
+                    },
+                });
+                return res.status(200).json('User has been unfollowed');
+            } else {
+                return res.status(403).json('You do not follow this user');
+            }
+        } catch {
+            return res.status(500).json(err);
+        }
+    } else {
+        return res.status(500).json("You can't unfollow yourself!");
+    }
+});
+
 module.exports = router;
