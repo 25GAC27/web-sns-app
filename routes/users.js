960914c0c344fc8fa7de9dadee50b5671800d111
watakazu-git <k25gac27@st.kyusan-u.ac.jp>
2025-06-14 16:04:16 +0900

    Add: ユーザー情報をDBに登録するAPIを作成

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-    res.send('users');
+// Register a new user
+router.post('/register', (req, res) => {
+    try {
+    } catch (err) {
+        return res.status(500).json(err);
+    }
 });
+
+// router.get('/', (req, res) => {
+//     res.send('users');
+// });
 
 module.exports = router;
