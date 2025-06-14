846b01280d9292739111876398f4fd8d2397732d
K.watanabe <k25gac27@st.kyusan-u.ac.jp>
2025-06-14 22:43:31 +0900

    Add: 投稿を編集するAPIを作成

--- routes/posts.js
+++ routes/posts.js
@@ -12,4 +12,21 @@ router.post('/', async (req, res) => {
     }
 });
 
+// update a post
+router.put('/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.userId === req.body.userId) {
+            await post.updateOne({
+                $set: req.body,
+            });
+            return res.status(200).json('Post updated successfully');
+        } else {
+            return res.status(403).json('You can only update your own posts!');
+        }
+    } catch (err) {
+        return res.status(403).json(err);
+    }
+});
+
 module.exports = router;
