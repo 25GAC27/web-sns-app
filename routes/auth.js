1e32a48c10a3b39216a9c890e94efc9d41608e30
watakazu-git <k25gac27@st.kyusan-u.ac.jp>
2025-06-14 16:46:01 +0900

    Add: ログイン用のAPIを作成

--- routes/auth.js
+++ routes/auth.js
@@ -17,6 +17,24 @@ router.post('/register', async (req, res) => {
     }
 });
 
+// Login a user
+router.post('/login', async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        if (!user) {
+            return res.status(404).send('User not found');
+        }
+        // Check password
+        const validPassword = req.body.password === user.password;
+        if (!validPassword) {
+            return res.status(400).json('Wrong password');
+        }
+        res.status(200).json(user);
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+});
+
 // router.get('/', (req, res) => {
 //     res.send('auth');
 // });
