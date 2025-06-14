b7135fbbfc6bbfd9ef51b586433e81f64b514e1c
watakazu-git <k25gac27@st.kyusan-u.ac.jp>
2025-06-14 15:53:36 +0900

    Update: MongoDBとNodeJSを接続

--- server.js
+++ server.js
@@ -4,6 +4,18 @@ const userRoute = require('./routes/users');
 const authRoute = require('./routes/auth');
 const postRoute = require('./routes/posts');
 const PORT = 3000;
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+// Connect to MongoDB
+mongoose
+    .connect(process.env.MONGOURL)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.error('MongoDB connection error:', err);
+    });
 
 // Middleware
 app.use('/api/users', userRoute);
