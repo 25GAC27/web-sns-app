428689fb13841bddec101beaadb3072c251280d9
watakazu-git <k25gac27@st.kyusan-u.ac.jp>
2025-06-14 17:48:54 +0900

    Add: 特定のユーザー情報を取得するAPIを作成

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,15 @@ const User = require('../models/User');
 // CRUD
 
 // Read a user
+router.get('/:id', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        const { password, updatedAt, ...other } = user._doc;
+        res.status(200).json(other);
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+});
 
 // Update a user
 router.put('/:id', async (req, res) => {
