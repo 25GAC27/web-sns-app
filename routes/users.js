a14e09ab9be982d16828c9c24e9fd674ceda44ce
watakazu-git <k25gac27@st.kyusan-u.ac.jp>
2025-06-14 16:20:47 +0900

    Add: APIを叩いてMongoDBにユーザーを追加する処理を追加

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,5 @@
 const router = require('express').Router();
 
-// Register a new user
-router.post('/register', (req, res) => {
-    try {
-    } catch (err) {
-        return res.status(500).json(err);
-    }
-});
-
 // router.get('/', (req, res) => {
 //     res.send('users');
 // });
